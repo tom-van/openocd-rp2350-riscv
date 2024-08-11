774b132bec565495532388092ed0b7fae1d1c2e0
Tomas Vanek <vanekt@fbl.cz>
2024-08-11 14:05:33 +0200

    target, riscv: examine the target when it becomes available
    Poll the target with TARGET_UNAVAILABLE state even if not examined.
    Emit TARGET_EVENT_BECOME_AVAILABLE which should be configured to
    issue Tcl command 'arp_examine' if needed.
    
    Signed-off-by: Tomas Vanek <vanekt@fbl.cz>
    Change-Id: I0476dafb712b99424f6ce011505d71ae9eb81f5e

--- src/target/target.c
+++ src/target/target.c
@@ -483,7 +483,7 @@ int target_poll(struct target *target)
 	int retval;
 
 	/* We can't poll until after examine */
-	if (!target_was_examined(target)) {
+	if (!target_was_examined(target) && target->state != TARGET_UNAVAILABLE) {
 		/* Fail silently lest we pollute the log */
 		return ERROR_FAIL;
 	}
@@ -2971,7 +2971,7 @@ static int handle_target(void *priv)
 			is_jtag_poll_safe() && target;
 			target = target->next) {
 
-		if (!target_was_examined(target))
+		if (!target_was_examined(target) && target->state != TARGET_UNAVAILABLE)
 			continue;
 
 		if (!target->tap->enabled)
